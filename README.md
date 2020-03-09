770dd8facf9d7a2ed575ae356c9bb16485c56783
Eugene Triguba <eugenetriguba@gmail.com>
2020-03-09 04:01:33 -0500

    Add personal details

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/66110d52-0eeb-450d-a7a0-61bc7d333fa9/deploy-status)](https://app.netlify.com/sites/jolly-yalow-7808b8/deploys)
+
+This is the source code of my personal website which I use as a portfolio and blog. It is made with [Gatsby]() using the [Minimal Blog theme](https://github.com/LekoArts/gatsby-starter-minimal-blog) and hosted with [Netlify]().
+
+The source code for this website is licensed under the [MIT License](https://opensource.org/licenses/MIT). The textual content on it is licensed under a [Creative Commons Attribution/Share-Alike License](http://creativecommons.org/licenses/by-sa/3.0/).
---
+++ gatsby-config.js
@@ -0,0 +1,70 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    siteTitleAlt: `Portfolio - Eugene Triguba`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Email`,
+            url: `mailto:eugenetriguba@gmail.com`,
+          },
+          {
+            name: `Github`,
+            url: `https://github.com/eugenetriguba`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Portfolio - Eugene Triguba`,
+        short_name: `Portfolio`,
+        description: `A Personal Portfolio.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-webpack-bundle-analyser-v2`,
+  ],
+}
